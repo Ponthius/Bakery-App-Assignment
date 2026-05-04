814a2fa1bea3eed1d7cb11a919f8180e671b91d6
Ponthius <ponthiuspaul@gmail.com>
2026-05-04 15:13:28 +0300

    Added SignUp validation

--- app/src/main/java/com/app/kasoone/Signup.java
+++ app/src/main/java/com/app/kasoone/Signup.java
@@ -3,6 +3,8 @@ package com.app.kasoone;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +24,40 @@ public class Signup extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    public static String savedUsername = "";
+    public static String savedPassword = "";
+
+    public void createAccount(View view){
+
+        EditText username = findViewById(R.id.etUsername);
+        EditText password = findViewById(R.id.etPassword);
+        EditText confirmPassword = findViewById(R.id.etConfirmPassword);
+
+        String user = username.getText().toString().trim();
+        String pass = password.getText().toString().trim();
+        String confirm = confirmPassword.getText().toString().trim();
+
+        if(user.isEmpty() || pass.isEmpty() || confirm.isEmpty()){
+            Toast.makeText(this, "Empty fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(!pass.equals(confirm)){
+            Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(user.equals(savedUsername)){
+            Toast.makeText(this, "User already exists", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        savedUsername = user;
+        savedPassword = pass;
+
+        Toast.makeText(this, "Account successfully created", Toast.LENGTH_SHORT).show();
     }
 
     public void gotoLogin(View view) {
